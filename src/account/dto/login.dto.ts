84007b6ead7223e1104d2f7ba313fb9adcfad165
emmatex <emmatex4yall@hotmail.com>
2021-02-06 00:20:00 +0100

    membership dto commit

--- src/account/dto/login.dto.ts
+++ src/account/dto/login.dto.ts
@@ -1,1 +1,10 @@
-export class LoginDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class LoginDto {
+    
+    @IsNotEmpty()
+    readonly email: string;
+    
+    @IsNotEmpty()
+    readonly password: string;
+}
