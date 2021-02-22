4991e77eedd81ed205271e2b47318c9dbd9d7d18
Johnson Iyida <kodili.johnson@gmail.com>
2021-02-22 15:18:46 +0100

    reactify organization  issues

--- src/account/AccountRepository.ts
+++ src/account/AccountRepository.ts
@@ -1,150 +1,182 @@
-import { EntityRepository, Repository } from "typeorm";
+import { EntityRepository, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
-import { AccountEntity } from "./entities/account.entity";
-import { RegisterDTO, LoginDTO, LockUserDTO } from "./dto/credential.dto";
-import { HttpException, HttpStatus } from "@nestjs/common";
-import { UserFromDbRO } from "./interfaces/account.interface";
-import { OrganizationRO, UserDataRO } from "./interfaces/user.interface";
-import { accountTypes } from "./account.constant";
+import { AccountEntity } from './entities/account.entity';
+import { RegisterDTO, LoginDTO, LockUserDTO } from './dto/credential.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { UserFromDbRO } from './interfaces/account.interface';
+import { OrganizationRO, UserDataRO } from './interfaces/user.interface';
+import { accountTypes } from './account.constant';
 
 @EntityRepository(AccountEntity)
 export class AccountRepository extends Repository<AccountEntity> {
-
-    public async register({ email, password, accountType, isRegComplete }: RegisterDTO): Promise<string> {
-        const isExists = await await this.findOne({ email });
-        if (isExists) {
-            throw new HttpException({ error: `${email} already exists`, status: HttpStatus.BAD_REQUEST },
-                HttpStatus.BAD_REQUEST);
-        }
-        const user = new AccountEntity();
-        user.email = email;
-        user.accountType = accountType;
-        user.createdBy = email;
-        user.accountPackage = 'Free';
-        user.isRegComplete = isRegComplete;
-        user.salt = await bcrypt.genSalt();
-        user.password = await this.hashPassword(password, user.salt);
-        try {
-            await user.save();
-            return 'Registration successful';
-        } catch (error) {
-            throw new HttpException({ error: `An error occured`, status: HttpStatus.INTERNAL_SERVER_ERROR },
-                HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  public async register({
+    email,
+    password,
+    accountType,
+    isRegComplete,
+  }: RegisterDTO): Promise<string> {
+    const isExists = await await this.findOne({ email });
+    if (isExists) {
+      throw new HttpException(
+        { error: `${email} already exists`, status: HttpStatus.BAD_REQUEST },
+        HttpStatus.BAD_REQUEST,
+      );
     }
-
-    public async validateUserPassword({ email, password }: LoginDTO): Promise<UserFromDbRO> {
-        const user = await await this.findOne({ where: { email: email } });
-        if (user && await user.validatePassword(password)) {
-            let data = {
-                email, accountPackage: user.accountPackage,
-                isRegComplete: user.isRegComplete, accountType: user.accountType
-            };
-            return data;
-        } else {
-            return null;
-        }
+    const user = new AccountEntity();
+    user.email = email;
+    user.accountType = accountType;
+    user.createdBy = email;
+    user.accountPackage = 'Free';
+    user.isRegComplete = isRegComplete;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    try {
+      await user.save();
+      return 'Registration successful';
+    } catch (error) {
+      throw new HttpException(
+        { error: `An error occured`, status: HttpStatus.INTERNAL_SERVER_ERROR },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 
-    private async hashPassword(password: string, salt: string): Promise<string> {
-        return bcrypt.hash(password, salt);
+  public async validateUserPassword({
+    email,
+    password,
+  }: LoginDTO): Promise<UserFromDbRO> {
+    const user = await await this.findOne({ where: { email: email } });
+    if (user && (await user.validatePassword(password))) {
+      let data = {
+        email,
+        accountPackage: user.accountPackage,
+        isRegComplete: user.isRegComplete,
+        accountType: user.accountType,
+      };
+      return data;
+    } else {
+      return null;
     }
+  }
 
-    public async getById(id: string): Promise<UserDataRO> {
-        const result = await this.findOne(id);
-        if (!result) {
-            throw new HttpException({
-                error: `user with id ${id} does not exists`, status: HttpStatus.NOT_FOUND
-            },
-                HttpStatus.NOT_FOUND);
-        }
-        return this.buildUserRO(result);
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+
+  public async getById(id: string): Promise<UserDataRO> {
+    const result = await this.findOne(id);
+    if (!result) {
+      throw new HttpException(
+        {
+          error: `user with id ${id} does not exists`,
+          status: HttpStatus.NOT_FOUND,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
+    return this.buildUserRO(result);
+  }
 
-    public async findByEmail(email: string): Promise<UserDataRO | undefined> {
-        const result = await this.findOne({ where: { email: email } });
-        if (!result) {
-            throw new HttpException({
-                error: `${email} does not exists`, status: HttpStatus.NOT_FOUND
-            }, HttpStatus.NOT_FOUND);
-        }
-        return this.buildUserRO(result);
+  public async findByEmail(email: string): Promise<UserDataRO | undefined> {
+    const result = await this.findOne({ where: { email: email } });
+    if (!result) {
+      throw new HttpException(
+        {
+          error: `${email} does not exists`,
+          status: HttpStatus.NOT_FOUND,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
+    return this.buildUserRO(result);
+  }
 
-    public async updateUser<T>(email: string, toUpdate: T): Promise<UserDataRO | undefined> {
-        const user = await this.findOne({ where: { email: email } });
-        if (!user) {
-            throw new HttpException({
-                error: `User with email: ${email} does not exists`, status: HttpStatus.NOT_FOUND
-            }, HttpStatus.NOT_FOUND);
-        }
-        const result = await this.merge(user, toUpdate);
-        await this.save(user);
-        return this.buildUserRO(result);
+  public async updateUser<T>(
+    email: string,
+    toUpdate: T,
+  ): Promise<UserDataRO | undefined> {
+    const user = await this.findOne({ where: { email: email } });
+    if (!user) {
+      throw new HttpException(
+        {
+          error: `User with email: ${email} does not exists`,
+          status: HttpStatus.NOT_FOUND,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
+    const result = await this.merge(user, toUpdate);
+    await this.save(user);
+    return this.buildUserRO(result);
+  }
 
-    public async lockAndUnlockUser({ email, isLocked }: LockUserDTO): Promise<UserDataRO | undefined> {
-        const user = await this.findOne({ where: { email: email } });
-        if (!user) {
-            throw new HttpException({
-                error: `User with email: ${email} does not exists`, status: HttpStatus.NOT_FOUND
-            }, HttpStatus.NOT_FOUND);
-        }
-        user.isLocked = isLocked;
-        const result = await await user.save();
-        return this.buildUserRO(result);
+  public async lockAndUnlockUser({
+    email,
+    isLocked,
+  }: LockUserDTO): Promise<UserDataRO | undefined> {
+    const user = await this.findOne({ where: { email: email } });
+    if (!user) {
+      throw new HttpException(
+        {
+          error: `User with email: ${email} does not exists`,
+          status: HttpStatus.NOT_FOUND,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
+    user.isLocked = isLocked;
+    const result = await await user.save();
+    return this.buildUserRO(result);
+  }
 
-    public async findOrg(): Promise<OrganizationRO[]> {
-        const accType = accountTypes.CORPORATE
-        const account = await this.createQueryBuilder("account")
-            .where("account.accountType) = (:accType)", { accType }).getMany();
-        return this.buildOrgArrRO(account);
-    }
+  public async findOrg(): Promise<OrganizationRO[] | undefined> {
+    const accType = accountTypes.CORPORATE;
+    const account = await this.find({ where: { accountType: accType, isRegComplete: true } });
+    return this.buildOrgArrRO(account);
+  }
 
-    private buildUserRO(user: AccountEntity) {
-        const userRO = {
-            id: user.id,
-            pcn: user.pcn,
-            email: user.email,
-            isLocked: user.isLocked,
-            lastName: user.lastName,
-            latitude: user.latitude,
-            firstName: user.firstName,
-            longitude: user.longitude,
-            phoneNumber: user.phoneNumber,
-            profileImage: user.profileImage,
-            accountPackage: user.accountPackage,
-            organizationName: user.organizationName,
-            organizationType: user.organizationType,
-            isRegComplete: user.isRegComplete,
-            accountType: user.accountType
-        };
-        return userRO;
-    }
+  private buildUserRO(user: AccountEntity) {
+    const userRO = {
+      id: user.id,
+      pcn: user.pcn,
+      email: user.email,
+      isLocked: user.isLocked,
+      lastName: user.lastName,
+      latitude: user.latitude,
+      firstName: user.firstName,
+      longitude: user.longitude,
+      phoneNumber: user.phoneNumber,
+      profileImage: user.profileImage,
+      accountPackage: user.accountPackage,
+      organizationName: user.organizationName,
+      organizationType: user.organizationType,
+      isRegComplete: user.isRegComplete,
+      accountType: user.accountType,
+    };
+    return userRO;
+  }
 
-    public buildUserArrRO(users: AccountEntity[]) {
-        let userArr = [];
-        users.forEach(user => {
-            userArr.push(this.buildUserRO(user));
-        })
-        return userArr;
-    }
+  public buildUserArrRO(users: AccountEntity[]) {
+    let userArr = [];
+    users.forEach((user) => {
+      userArr.push(this.buildUserRO(user));
+    });
+    return userArr;
+  }
 
-    private buildOrgArrRO(users: AccountEntity[]) {
-        let userArr = [];
-        users.forEach(user => {
-            const userRO = {
-                pcn: user.pcn,
-                latitude: user.latitude,
-                longitude: user.longitude,
-                organizationName: user.organizationName,
-                organizationType: user.organizationType,
-                companyRegistrationNumber: user.companyRegistrationNumber
-            };
-            userArr.push(userRO);
-        })
-        return userArr;
-    }
-
+  private buildOrgArrRO(users: AccountEntity[]) {
+    let userArr = [];
+    users.forEach((user) => {
+      const userRO = {
+        pcn: user.pcn,
+        latitude: user.latitude,
+        longitude: user.longitude,
+        organizationName: user.organizationName,
+        organizationType: user.organizationType,
+        companyRegistrationNumber: user.companyRegistrationNumber,
+      };
+      userArr.push(userRO);
+    });
+    return userArr;
+  }
 }
