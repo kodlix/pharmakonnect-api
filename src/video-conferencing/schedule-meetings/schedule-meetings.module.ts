b99bb06b6b16e40f58a50b2259cc0da085c9d5f5
Johnson Iyida <kodili.johnson@gmail.com>
2021-03-04 11:08:30 +0100

    merge schedule meeting

--- src/video-conferencing/schedule-meetings/schedule-meetings.module.ts
+++ src/video-conferencing/schedule-meetings/schedule-meetings.module.ts
@@ -3,10 +3,11 @@ import { ScheduleMeetingsService } from './schedule-meetings.service';
 import { ScheduleMeetingsController } from './schedule-meetings.controller';
 import { ScheduleMeetingRepository } from './schedule-meeting.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountModule } from 'src/account/account.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ScheduleMeetingRepository]),
+    TypeOrmModule.forFeature([ScheduleMeetingRepository]),AccountModule
   ],
   controllers: [ScheduleMeetingsController],
   providers: [ScheduleMeetingsService]
