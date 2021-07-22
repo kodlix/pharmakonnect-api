7a93e489bdcf8b39e9de388999c052a62e0bbb32
Johnson Iyida <kodili.johnson@gmail.com>
2021-07-22 11:37:40 +0100

    remove unnessary properties fron contact

--- src/contact/entities/contact.entity.ts
+++ src/contact/entities/contact.entity.ts
@@ -3,24 +3,10 @@ import { AbstractBaseEntity } from "src/_common/base.entity";
 
 @Entity("Contact")
 export class ContactEntity extends AbstractBaseEntity {
-
-
     @Column()
     creatorId: string
 
     @Column()
     accountId: string
-
-    @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    phoneNo: string
-
-    @Column()
-    email: string
 
 }
