f907534bc3279902a0b4f44deff152cad3e00efa
Johnson Iyida <kodili.johnson@gmail.com>
2021-07-22 12:20:56 +0100

    update contact

--- src/contact/entities/contact.entity.ts
+++ src/contact/entities/contact.entity.ts
@@ -1,12 +1,25 @@
-import { Column, Entity, ManyToOne } from "typeorm";
+import { Column, Entity } from "typeorm";
 import { AbstractBaseEntity } from "src/_common/base.entity";
 
 @Entity("Contact")
 export class ContactEntity extends AbstractBaseEntity {
+
     @Column()
     creatorId: string
 
     @Column()
     accountId: string
+
+    @Column({nullable: true})
+    firstName: string
+
+    @Column({nullable: true})
+    lastName: string
+
+    @Column({ nullable: true})
+    phoneNo: string
+
+    @Column({ nullable: true})
+    email: string
 
 }
